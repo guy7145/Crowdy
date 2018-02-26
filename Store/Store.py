d792ae78ccf07755bee52924e8bacd1e488ff5b0
MiaoHatola <jalon0@gmail.com>
2018-02-27 00:40:05 +0200

    Something

--- Store/Store.py
+++ Store/Store.py
@@ -1,21 +1,26 @@
 import subprocess,json,time,sys
 from multiprocessing.pool import Pool
-import grequests
+from Data.http import fetch_url
 cmd = "zbarcam.exe --nodisplay"
-def run(host):
-    from subprocess import Popen, PIPE, CalledProcessError
-    with Pool() as pool:
-        with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-            for line in p.stdout:
-                if(line == ""):
-                    print("Hello")
-                # print(line, end='') # process line here
-                data = json.loads(line.split(":")[1])
-                pool.apply_async(handle,[data])
 
 def handle(data):
     print(data)
-    grequests.get(host + ':8080/scannedQR?data=' + data)
+    global host
+    fetch_url(host + ':8080/scannedQR?data=' + data)
+
+def run():
+    from subprocess import Popen, PIPE, CalledProcessError
+    # with Pool() as pool:
+    with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            if(line == ""):
+                continue
+            data = json.loads(line[line.find(":")+1:])
+            # pool.apply_async(handle,[data])
+            handle(data)
+
 
 if __name__=="__main__":
-    run(sys.argv[0])
+    global host
+    host=sys.argv[1]
+    run()
